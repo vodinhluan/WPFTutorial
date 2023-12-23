8452d5d568203f04bf7bbf820b7231a3b27892ae
vodinhluan <luandinhvo2002@gmail.com>
2023-12-23 15:31:14 +0700

    Add project files.

---
+++ WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                // stop
+                tbTitle.Text = "FIFA ONLINE";
+                btnStart.Content = "START";
+
+            } else
+            {
+                // run
+                tbTitle.Text = "WELCOME";
+                btnStart.Content = "BACK";
+            }
+            
+            running = !running;
+        }
+    }
+
+    
+}
